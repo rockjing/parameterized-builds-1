b64b7482ddeb14f96d9cdb016a347861ce27ec6d
a-hacker <explosivepyrounicorns@gmail.com>
2019-02-17 19:51:43 -0500

    Add different param types

--- parameterized-client/src/manual/build-dialog.js
+++ parameterized-client/src/manual/build-dialog.js
@@ -8,6 +8,7 @@ export default class BuildDialog extends React.Component {
             job: props.jobs[0]
         };
         this.selectJob = this.selectJob.bind(this);
+        this.createParamInput = this.createParamInput.bind(this);
     }
 
     selectJob(event) {
@@ -16,16 +17,41 @@ export default class BuildDialog extends React.Component {
         });
     }
 
+    createParamInput(id, val){
+        if (typeof val === "boolean"){
+            return (
+                <input id={"build-param-value-" + id} className="checkbox"
+                       name={"build-param-value-" + id} defaultChecked={val}
+                       type="checkbox"/>
+            );
+        } else if (typeof val === "string") {
+            return (
+                <input id={"build-param-value-" + id} className="text"
+                       name={"build-param-value-" + id} value={val}
+                       type="text"/>
+            );
+        } else {
+            const options = val.map(opt => <option value={opt}>{opt}</option>)
+            return (
+                <select id={"build-param-value-" + id} className="select">
+                    {options}
+                </select>
+            )
+        }
+    }
+
     render() {
         const jobOptions = this.props.jobs.map(job => <option value={job.id}>{job.jobName}</option>);
         let paramNum = 0;
         let jobParams = this.state.job.buildParameters.map(param => {
+            const paramName = Object.keys(param)[0];
+            const paramVal = Object.values(param)[0];
+            const paramInput = this.createParamInput(paramNum, paramVal);
+
             let paramDiv = (
                 <div id={"jenkins-form-" + paramNum} className="field-group jenkins-form">
-                    <label htmlFor={"build-param-value-" + paramNum}>{Object.keys(param)[0]}</label>
-                    <input id={"build-param-value-" + paramNum} className="text" 
-                           name={"build-param-value-" + paramNum} value={Object.values(param)[0]}
-                           type="text"/>
+                    <label htmlFor={"build-param-value-" + paramNum}>{paramName}</label>
+                    {paramInput}
                 </div>
             );
             paramNum++;
@@ -44,7 +70,7 @@ export default class BuildDialog extends React.Component {
                 <div className="aui-dialog2-content">
                     <form className="aui" action="" method="post">
                         <select id="job" className="select" name="job" style={{"maxWidth":"initial", "width":"auto"}} 
-                                value={this.state.job} onChange={this.selectJob}>
+                                value={this.state.job.id} onChange={this.selectJob}>
                             {jobOptions}
                         </select>
                         <div className="job-params">
